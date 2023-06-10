bd2f238b37c0e61ec284ab69f99c592fe842d4bf
Zedrick Torres <torres.zsj@gmail.com>
2023-06-10 23:54:09 +0800

    Finish Exercise 10.12

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { NativeRouter } from "react-router-native";
+import { ApolloProvider } from "@apollo/client";
+import Constants from "expo-constants";
 
 import Main from "./src/components/Main";
-import { ApolloProvider } from "@apollo/client";
 import createApolloClient from "./src/utils/apolloClient";
 
 const apolloClient = createApolloClient();
 
 const App = () => {
+  console.log(Constants.manifest);
+
   return (
     <>
       <NativeRouter>
